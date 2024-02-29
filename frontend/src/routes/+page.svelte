72939e1c91cec401f229389f4269b5b831f49f9c
Vajda Máté Pál <vajdamate0204@gmail.com>
2024-02-29 17:51:56 +0100

    fix: Fixing language for thesis

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,14 +2,14 @@
     import Button from '$lib/components/ui/button/button.svelte';
 
 
-    import Centered from '$lib/centered.svelte';
+    import Centered from '$lib/Centered.svelte';
     import BottleImage from '$lib/assets/Wine Bottle.png'
 </script>
 
 <div class="h-screen flex">
     <Centered>
-        <Button class="text-xl lg:text-5xl text-slate-400" variant="link"> 
-            Register a Wine
+        <Button class="text-xl lg:text-5xl text-slate-400" variant="link" href="/register"> 
+            Regisztrálj egy üveget
         </Button>
     </Centered>
     
@@ -18,8 +18,8 @@
     </Centered>
     
     <Centered>
-        <Button class="text-xl lg:text-5xl text-slate-400" variant="link"> 
-            Check a Wine
+        <Button class="text-xl lg:text-5xl text-slate-400" variant="link" href="/check"> 
+            Ellenőrizz egy üveget
         </Button>
     </Centered>
 </div> 

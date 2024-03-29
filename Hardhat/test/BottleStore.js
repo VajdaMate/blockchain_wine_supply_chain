54deb9289662a78de2b55e1c912d7f92c4749c27
VajdaMate <vajdamate0204@gmail.com>
2024-03-29 09:42:00 +0100

    feat: Polished testing

--- Hardhat/test/BottleStore.js
+++ Hardhat/test/BottleStore.js
@@ -4,21 +4,19 @@ const { ethers } = require('hardhat');
 
 describe('BottleStore contract with Gas Reporter', () => {
   let contract;
-  let deployer;
+  const grapeType = 'Chardonnay';
 
   beforeEach(async () => {
-    [deployer] = await ethers.getSigners();
+    
     contract =await ethers.deployContract("BottleStore");
     await contract.waitForDeployment();
   });
 
   describe('registerBottle', () => {
     it('registers a new bottle', async () => {
-      const grapeType = 'Sauvignon Blanc';
       await contract.registerBottle(grapeType);
       const firstBottleId = await contract.returnLastBottleID();  
       const bottle = await contract.returnBottleByID(firstBottleId);
-
 
       expect(bottle.typeOfGrape).to.equal(grapeType);
     });
@@ -26,7 +24,6 @@ describe('BottleStore contract with Gas Reporter', () => {
 
   describe('returnLastBottleID', () => {
     it('returns the ID of the last bottle', async () => {
-      const grapeType = 'Tempranillo';
       await contract.registerBottle(grapeType);
       const lastBottleId = await contract.returnLastBottleID();
       expect(lastBottleId).to.be.equal(0);
@@ -35,8 +32,6 @@ describe('BottleStore contract with Gas Reporter', () => {
 
   describe('returnBottleByID', () => {
     it('returns bottle information by ID', async () => {
-      const grapeType = 'Malbec';
-
       await contract.registerBottle(grapeType);
       const firstBottleId = await contract.returnLastBottleID();
       const bottle = await contract.returnBottleByID(firstBottleId);
@@ -47,20 +42,24 @@ describe('BottleStore contract with Gas Reporter', () => {
 
   describe('ownersBottles', () => {
     it('returns all bottles owned by the caller', async () => {
-      const grapeType1 = 'Carmenere';
-      const grapeType2 = 'Grenache';
+      const grapeType1 = 'Cuvéé';
+      const grapeType2 = 'Irsai';
 
       await contract.registerBottle(grapeType1);
       await contract.registerBottle(grapeType2);
 
       const ownedBottles = await contract.ownersBottles();
-      expect(ownedBottles).to.be.an('array');
+      
+      expect(ownedBottles[0]).to.be.an('array');
+      
+      expect(ownedBottles[0].typeOfGrape).to.equal(grapeType1);
+      expect(ownedBottles[1].typeOfGrape).to.equal(grapeType2);
     });
   });
 
+
   describe('updateSunnyHours', () => {
     it('updates sunny hours for a bottle', async () => {
-        const grapeType = 'Chardonnay';
         const sunnyHours = 10;
     
         await contract.registerBottle(grapeType);
@@ -75,7 +74,6 @@ describe('BottleStore contract with Gas Reporter', () => {
 
   describe('updateRainMilimeters', () => {
     it('updates rain milimeters for a bottle', async () => {
-        const grapeType = 'Pinot Noir';
         const rainMilimeters = 20;
     
         await contract.registerBottle(grapeType);
@@ -88,31 +86,29 @@ describe('BottleStore contract with Gas Reporter', () => {
   });
 
     describe('updateTimeOfHarvest', () => {
-        it('updates time of harvest for a bottle', async () => {
-            const grapeType = 'Merlot';
-            const timeOfHarvest = "2022";
-        
-            await contract.registerBottle(grapeType);
-            const firstBottleId = await contract.returnLastBottleID();
-            await contract.updateTimeOfHarvest(parseInt(firstBottleId), timeOfHarvest);
-            let bottle=await contract.returnBottleByID(parseInt(firstBottleId));
-        
-            expect(bottle.timeOfHarvest).to.equal(timeOfHarvest);
-        });
+      it('updates time of harvest for a bottle', async () => {
+          const timeOfHarvest = "2023.09.01";
+      
+          await contract.registerBottle(grapeType);
+          const firstBottleId = await contract.returnLastBottleID();
+          await contract.updateTimeOfHarvest(parseInt(firstBottleId), timeOfHarvest);
+          let bottle=await contract.returnBottleByID(parseInt(firstBottleId));
+      
+          expect(bottle.timeOfHarvest).to.equal(timeOfHarvest);
+      });
     });
 
     describe('updateTimeOfBottling', () => {
-        it('updates time of bottling for a bottle', async () => {
-            const grapeType = 'Syrah';
-            const timeOfBottling = "2023";
-        
-            await contract.registerBottle(grapeType);
-            const firstBottleId = await contract.returnLastBottleID();
-            await contract.updateTimeOfBottling(parseInt(firstBottleId), timeOfBottling);
-            let bottle=await contract.returnBottleByID(parseInt(firstBottleId));
-        
-            expect(bottle.timeOfBottling).to.equal(timeOfBottling);
-        });
+      it('updates time of bottling for a bottle', async () => {
+          const timeOfBottling = "2024.06.01";
+      
+          await contract.registerBottle(grapeType);
+          const firstBottleId = await contract.returnLastBottleID();
+          await contract.updateTimeOfBottling(parseInt(firstBottleId), timeOfBottling);
+          let bottle=await contract.returnBottleByID(parseInt(firstBottleId));
+      
+          expect(bottle.timeOfBottling).to.equal(timeOfBottling);
+      });
 
     });
 });

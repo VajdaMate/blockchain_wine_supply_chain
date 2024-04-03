22762ed2e1527d1cfd30e92417af4679ec77b0b1
VajdaMate <vajdamate0204@gmail.com>
2024-04-03 14:58:40 +0200

    feat: Version 3 of the smart contract, major change in logic

--- Hardhat/contracts/BottleStore.sol
+++ Hardhat/contracts/BottleStore.sol
@@ -6,10 +6,10 @@ contract BottleStore{
         address owner;
         uint256 bottleId;
         string typeOfGrape;
-        uint256 sunnyHours;
-        uint256 rainMilimeters;
-        string timeOfHarvest;
-        string timeOfBottling;
+        uint32[] sunnyHours;
+        uint32[] rainMilimeters;
+        string[] timeOfHarvest;
+        string[] timeOfBottling;
     }
     
     Bottle[] public bottles;
@@ -17,7 +17,16 @@ contract BottleStore{
     
     //Registerhez
     function registerBottle(string memory _typeOfGrape) public {
-        bottles.push(Bottle(msg.sender,bottles.length,_typeOfGrape,0,0,"M\u00e9g nem sz\u00fcretelt","M\u00e9g nem palackozott"));
+        uint32[] memory tmpSunny = new uint32[](0); 
+        tmpSunny[0]=0; 
+        uint32[] memory tmpRain = new uint32[](0); 
+        tmpRain[0]=0;
+        string[] memory tmpTimeOfHarvest = new string[](0);
+        tmpTimeOfHarvest[0]="M\u00e9g nem sz\u00fcretelt";
+        string[] memory tmpTimeOfBottling = new string[](0);
+        tmpTimeOfBottling[0]="M\u00e9g nem palackozott";
+
+        bottles.push(Bottle(msg.sender,bottles.length,_typeOfGrape,tmpSunny,tmpRain,tmpTimeOfHarvest,tmpTimeOfBottling));
     }
 
     function returnLastBottleID()public view returns (uint256){
@@ -55,33 +64,35 @@ contract BottleStore{
 
     //Updatehez
 
-    function updateSunnyHours(uint256 _id, uint256 _sunnyHours) public {
+    function updateSunnyHours(uint256 _id, uint32 _sunnyHours) public {
         require(bottles[_id].owner == msg.sender, "Csak saj\u00E1t \u00FCveget friss\u00EDthetsz!"); //Unicode karakterek
-        require(bottles[_id].sunnyHours==0, 
-        "Ha egyszer meg lett adva a naps\\u00FCt\\u00E9ses \\u00F3r\\u00E1k sz\\u00E1ma, nem lehet friss\\u00EDteni!");
-        bottles[_id].sunnyHours=_sunnyHours;
+        
+        bottles[_id].sunnyHours.push(_sunnyHours);
     }
 
-    function updateRainMilimeters(uint256 _id, uint256 _rainMilimeters) public {
+    function updateRainMilimeters(uint256 _id, uint32 _rainMilimeters) public {
         require(bottles[_id].owner == msg.sender, "Csak saj\u00E1t \u00FCveget friss\u00EDthetsz!");
-        require(bottles[_id].rainMilimeters==0, 
-        "Ha egyszer meg lett adva az es\\u0151 mennyis\\u00E9ge, nem lehet friss\\u00EDteni!");
-        bottles[_id].rainMilimeters=_rainMilimeters;
+       
+        bottles[_id].rainMilimeters.push(_rainMilimeters);
     }
 
     function updateTimeOfHarvest(uint256 _id, string memory _timeOfHarvest) public {
         require(bottles[_id].owner == msg.sender, "Csak saj\u00E1t \u00FCveget friss\u00EDthetsz!");
-        require(keccak256(abi.encodePacked(bottles[_id].timeOfHarvest)) == keccak256(abi.encodePacked("M\u00e9g nem sz\u00fcretelt")), 
-        "Ha egyszer meg lett adva a sz\\u00FCret ideje, nem lehet friss\\u00EDteni!");
-        bottles[_id].timeOfHarvest=_timeOfHarvest;
+        bottles[_id].timeOfHarvest.push(_timeOfHarvest);
     }
 
     function updateTimeOfBottling(uint256 _id, string memory _timeOfBottling) public {
         require(bottles[_id].owner == msg.sender, "Csak saj\u00E1t \u00FCveget friss\u00EDthetsz!");
-        require(keccak256(abi.encodePacked(bottles[_id].timeOfBottling)) == keccak256(abi.encodePacked("M\u00e9g nem palackozott")), 
-        "Ha egyszer meg lett adva a palackoz\\u00E1s ideje, nem lehet friss\\u00EDteni!");
-        bottles[_id].timeOfBottling=_timeOfBottling;
+      
+        bottles[_id].timeOfBottling.push(_timeOfBottling);
     }
 
 
+    //Tulajváltás
+    function giveBottle(uint256 _id, address _newOwner) public {
+        require(bottles[_id].owner == msg.sender, "Csak saj\u00E1t \u00FCveget adhatsz!");
+        bottles[_id].owner = _newOwner;
+    }
+
+    
 }

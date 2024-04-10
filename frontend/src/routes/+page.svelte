06f1f2b3f97763b4b6214d180316d74b3fc03372
VajdaMate <vajdamate0204@gmail.com>
2024-04-10 14:19:16 +0200

    feat: Add all page options

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,22 +2,33 @@
     import Button from '$lib/components/ui/button/button.svelte';
     import RowCentered from '$lib/RowCentered.svelte';
     import BottleImage from '$lib/assets/Wine Bottle.png'
+    import ColCentered from '$lib/ColCentered.svelte';
 </script>
 
 <RowCentered>
-    <div>
-        <Button class="text-xl lg:text-5xl text-slate-400" variant="link" href="/register"> 
+    <ColCentered>
+        <Button class="text-xl lg:text-5xl text-slate-400 mb-20" variant="link" href="/register"> 
             Regisztrálj egy üveget
         </Button>
-    </div>
+        
+        <Button class="text-xl lg:text-5xl text-slate-400" variant="link" href="/transfer">
+            Ruházd át tulajdonjogod
+        </Button>
+
+    </ColCentered>
     
     <RowCentered>
        <img class="h-full w-full object-cover" src={BottleImage} alt="Wine Bottle" />
     </RowCentered>
     
-    <div>
-        <Button class="text-xl lg:text-5xl text-slate-400" variant="link" href="/check"> 
+    <ColCentered>
+        <Button class="text-xl lg:text-5xl text-slate-400 mb-20" variant="link" href="/check"> 
             Ellenőrizz egy üveget
         </Button>
-    </div>
+
+        <Button class="text-xl lg:text-5xl text-slate-400" variant="link" href="/update">
+           Frissíts egy üveget
+        </Button>
+
+    </ColCentered>
 </RowCentered>
